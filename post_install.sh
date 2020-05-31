57ec602853f1720c82416c5b8c59d45c7c7b196c
Malte F. Hillmann <info@maltehillmann.de>
2020-05-31 23:54:14 +0200

    Changed some cosmetics
    No new lines on re/starts
    Hide curl output

--- post_install.sh
+++ post_install.sh
@@ -6,14 +6,14 @@ sysrc -f /etc/rc.conf influxd_enable="YES" 2>/dev/null
 sysrc -f /etc/rc.conf grafana_enable="YES" 2>/dev/null
 
 # Start the services
-echo "Starting InfluxDB..."
+echo -n "Starting InfluxDB..."
 service influxd start 2>/dev/null
 curl -sL -I localhost:8086/ping?wait_for_leader=30s
-echo "Done"
-echo "Starting Grafana..."
+echo " done"
+echo -n "Starting Grafana..."
 service grafana start 2>/dev/null
 while [ "$(curl -I -s -o /dev/null -w 200 http://localhost:3000/api/health)" != "200" ]; do sleep 1; done
-echo "Done"
+echo " done"
 
 # Configure InfluxDB
 echo "Configuring InfluxDB:"
@@ -25,27 +25,27 @@ echo "Creating default database grafana"
 influx -execute "CREATE DATABASE grafana"
 echo "Enabling auth"
 sed -i.conf 's/# auth-enabled = false/auth-enabled = true/g' /usr/local/etc/influxd.conf
-echo "Restarting InfluxDB..."
+echo -n "Restarting InfluxDB..."
 service influxd restart 2>/dev/null
-curl -sL -I localhost:8086/ping?wait_for_leader=30s
-echo "Done"
+curl -sL -I 'http://localhost:8086/ping?wait_for_leader=30s' > /dev/null
+echo " done"
 
 # Configure Grafana
 echo "Configuring Grafana:"
 echo "Creating random password"
 GRAFANAPW=$(openssl rand -base64 18)
 echo "Configuring InfluxDB as default datasource"
-curl --user admin:admin 'http://localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"InfluxDB","isDefault":true,"type":"influxdb","url":"http://localhost:8086","access":"proxy","basicAuth":true,"basicAuthUser":"root","database":"grafana","secureJsonData":{"basicAuthPassword":"'$INFLUXPW'"}}'
+curl --silent --user admin:admin 'http://localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"InfluxDB","isDefault":true,"type":"influxdb","url":"http://localhost:8086","access":"proxy","basicAuth":true,"basicAuthUser":"root","database":"grafana","secureJsonData":{"basicAuthPassword":"'$INFLUXPW'"}}' > /dev/null
 echo "Setting random generated admin password"
-curl --user admin:admin 'http://localhost:3000/api/admin/users/1/password' -X PUT -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"password":"'$GRAFANAPW'"}'
+curl --silent --user admin:admin 'http://localhost:3000/api/admin/users/1/password' -X PUT -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"password":"'$GRAFANAPW'"}' > /dev/null
 echo "Creating missing directorys"
 mkdir -p /var/db/grafana/provisioning/datasources 
 mkdir -p /var/db/grafana/provisioning/notifiers
 mkdir -p /var/db/grafana/provisioning/dashboards
-echo "Restarting Grafana..."
+echo -n "Restarting Grafana..."
 service grafana restart 2>/dev/null
-while [ "$(curl -I -s -o /dev/null -w 200 http://localhost:3000/api/health)" != "200" ]; do sleep 1; done
-echo "Done"
+while [ "$(curl -I -s -o /dev/null -w 200 'http://localhost:3000/api/health')" != "200" ]; do sleep 1; done
+echo " done"
 
 
 # Save data
